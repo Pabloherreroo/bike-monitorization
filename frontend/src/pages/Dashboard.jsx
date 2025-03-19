34d5898ae95a4864fc536a6f0a66de1d072620e5
Pablo Herrero <pabloherrero@opendeusto.es>
2025-03-19 19:31:26 +0100

    Estado de bicicletas
    Dashboard semi completo, solo quedan alertas o cambiarlo por una funcion del superadmin
    - Se ha incluido una lista de bicis diferenciando entre las de prueba BP y las normales B
    - Iconos de colores y estado visual, barra vertical para desplazamiento

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -115,6 +115,10 @@ const Dashboard = ({ bikeData, bikes }) => {
         return "worst-indicator";
     };
 
+    const isBikeTest = (bikeId) => {
+        return /^BP\d+$/.test(bikeId)
+    }
+
     return (
         <div>
             <h2>Dashboard</h2>
@@ -219,11 +223,36 @@ const Dashboard = ({ bikeData, bikes }) => {
                         </div>
                     </div>
                 </div>
-                <div className="dashboard-card">
-                    <span>Alertas</span>
+                <div className="dashboard-card-container">
+                    <h3 className="dashboard-card-title">Alertas</h3>
+                    <div className="dashboard-card alert-card">
+                        
+                    </div>
                 </div>
-                <div className="dashboard-card">
-                    <span>Estatus de bicicletas</span>
+                <div className="dashboard-card-container">
+                    <h3 className="dashboard-card-title">Estado de bicicletas</h3>
+                    <div className="dashboard-card estado-card">
+                        <div className="bikes-list-container">
+                            {bikes && bikes.length > 0 ? (
+                                bikes.map((bike, index) => (
+                                    <div key={index} className="bike-row">
+                                        <div className={`bike-icon ${isBikeTest(bike.bike_id) ? 'test-bike-icon' : 'normal-bike-icon'}`}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+                                                <path d="M18.18 10l-1.7-4.68A2.008 2.008 0 0 0 14.6 4H12v2h2.6l1.46 4h-4.81l-.36-1H12V7H7v2h1.75l1.82 5H9.9c-.44-2.23-2.31-3.88-4.65-3.99C2.45 9.87 0 12.2 0 15c0 2.8 2.2 5 5 5 2.46 0 4.45-1.69 4.9-4h4.2c.44 2.23 2.31 3.88 4.65 3.99 2.8.13 5.25-2.19 5.25-5 0-2.8-2.2-5-5-5h-.82zM7.82 16c-.4 1.17-1.49 2-2.82 2-1.68 0-3-1.32-3-3s1.32-3 3-3c1.33 0 2.42.83 2.82 2H5v2h2.82zm6.28-2h-1.4l-.73-2H15c-.44.58-.76 1.25-.9 2zm4.9 4c-1.68 0-3-1.32-3-3 0-.93.41-1.73 1.05-2.28l.96 2.64 1.88-.68-.97-2.67c.03 0 .06-.01.09-.01 1.68 0 3 1.32 3 3s-1.33 3-3.01 3z"/>
+                                            </svg>
+                                        </div>
+                                        <div className="bike-name">{bike.bike_id}</div>
+                                        <div className={`bike-status ${bike.estado === 'en funcionamiento' ? 'status-active' : 'status-inactive'}`}>
+                                            <span>{bike.estado === 'en funcionamiento' ? 'En movimiento' : 'Parada'}</span>
+                                            <div className={`status-indicator ${bike.estado === 'en funcionamiento' ? 'indicator-active' : 'indicator-inactive'}`}></div>
+                                        </div>
+                                    </div>
+                                ))
+                            ) : (
+                                <div className="no-bikes-message">No hay datos de bicicletas disponibles</div>
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
