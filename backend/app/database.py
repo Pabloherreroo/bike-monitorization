360ab89e991528c640f66ae407f9b2799c86ae96
Pablo Herrero <pabloherrero@opendeusto.es>
2025-03-23 12:32:44 +0100

    BD y Docker
    - Dockerizado el proyecto al completo
    - Init y populate se ejecutan cada vez que se inicia el proyecto, pero no sobreescriben los datos si ya estaban ni vacian las tablas

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/bicicleta_data"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/bicicleta_data")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"client_encoding": "UTF8"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- db/init.sql
+++ db/init.sql
@@ -1,37 +1,43 @@
-DROP DATABASE IF EXISTS bicicleta_data;
-CREATE DATABASE bicicleta_data 
+SELECT 'CREATE DATABASE bicicleta_data
     WITH OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'es_ES.UTF-8'
-    LC_CTYPE = 'es_ES.UTF-8'
-    TEMPLATE = template0;
+    ENCODING = ''UTF8''
+    LC_COLLATE = ''es_ES.UTF-8''
+    LC_CTYPE = ''es_ES.UTF-8''
+    TEMPLATE = template0'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'bicicleta_data');
 
 \c bicicleta_data;
 SET client_encoding TO 'UTF8';
 
 CREATE EXTENSION IF NOT EXISTS timescaledb;
 
-CREATE TABLE bikes (
+CREATE TABLE IF NOT EXISTS bikes (
     id SERIAL PRIMARY KEY,
     bike_id TEXT UNIQUE NOT NULL,
     estado VARCHAR(20) CHECK (estado IN ('en funcionamiento', 'parada')) NOT NULL,
     fecha_registro TIMESTAMPTZ DEFAULT NOW()
 );
 
-CREATE TABLE bike_data (
-    bike_id TEXT NOT NULL,  
-    latitud DOUBLE PRECISION NOT NULL,
-    longitud DOUBLE PRECISION NOT NULL,
-    puntuacion_road INT CHECK (puntuacion_road >= 1 AND puntuacion_road <= 4) NOT NULL,
-    calidad_ambiental INT CHECK (calidad_ambiental >= 0 AND calidad_ambiental <= 100) NOT NULL,
-    ruido INT CHECK (ruido >= 0 AND ruido <= 120) NOT NULL,
-    barrio TEXT,
-    fecha TIMESTAMPTZ NOT NULL,
-    PRIMARY KEY (bike_id, fecha),  -- Clave primaria compuesta por bike_id y fecha
-    CONSTRAINT fk_bike_id FOREIGN KEY (bike_id) REFERENCES bikes(bike_id)
-);
-
--- Crear la 'hypertable' para 'bike_data' con partición en 'fecha'
-SELECT create_hypertable('bike_data', 'fecha');
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_tables WHERE tablename = 'bike_data') THEN
+        CREATE TABLE bike_data (
+            bike_id TEXT NOT NULL,  
+            latitud DOUBLE PRECISION NOT NULL,
+            longitud DOUBLE PRECISION NOT NULL,
+            puntuacion_road INT CHECK (puntuacion_road >= 1 AND puntuacion_road <= 4) NOT NULL,
+            calidad_ambiental INT CHECK (calidad_ambiental >= 0 AND calidad_ambiental <= 100) NOT NULL,
+            ruido INT CHECK (ruido >= 0 AND ruido <= 120) NOT NULL,
+            barrio TEXT,
+            fecha TIMESTAMPTZ NOT NULL,
+            PRIMARY KEY (bike_id, fecha), --PK compuesta por el id y la fecha
+            CONSTRAINT fk_bike_id FOREIGN KEY (bike_id) REFERENCES bikes(bike_id)
+        );
+        
+        -- Crear hypertable para bike_data fecha
+        PERFORM create_hypertable('bike_data', 'fecha');
+    END IF;
+END
+$$;
 
 
